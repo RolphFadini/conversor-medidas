df255389d02fc507a3e25458e95d1b32882de260
Rolph Fadini <rolphfadini@hotmail.com>
2023-11-22 14:54:52 -0300

    Add case 2 in App.java

--- src/main/java/br/com/infnet/App.java
+++ src/main/java/br/com/infnet/App.java
@@ -1,12 +1,14 @@
 package br.com.infnet;
 
 import br.com.infnet.util.ConversorDeTemperaturas;
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -31,15 +33,38 @@ public class App {
         switch(scanner.nextInt()){
             case 1: {
                 System.out.println("Digite a temperatura em C");
-                double celsius = scanner.nextDouble();
-                ConversorDeTemperaturas conversorDeTemperaturas = new ConversorDeTemperaturas();
-                LocalDateTime start = LocalDateTime.now();
-                LOGGER.info("Inicio do calculo de conversão");
-                double kelvin = conversorDeTemperaturas.celsiusParaKelvin(celsius);
-                LocalDateTime end = LocalDateTime.now();
-                long between = ChronoUnit.MILLIS.between(start, end);
-                LOGGER.info("Final do metodo de conversão com tempo: " + between + " MS");
-                System.out.println("A temperatura em K é: " + kelvin + "K");
+                try {
+                    double celsius = scanner.nextDouble();
+                    ConversorDeTemperaturas conversorDeTemperaturas = new ConversorDeTemperaturas();
+                    LocalDateTime start = LocalDateTime.now();
+                    LOGGER.info("Inicio do calculo de conversão");
+                    double kelvin = conversorDeTemperaturas.celsiusParaKelvin(celsius);
+                    LocalDateTime end = LocalDateTime.now();
+                    long between = ChronoUnit.MILLIS.between(start, end);
+                    LOGGER.debug("Final do metodo de conversão com tempo: " + between + " MS" + " para o valor: " + celsius);
+                    System.out.println("A temperatura em K é: " + kelvin + "K");
+                } catch(InputMismatchException e) {
+                    LOGGER.error("Valor inválido para temperatura");
+                    System.out.println("Valor inválido para temperatura");
+                }
+                break;
+            }
+            case 2: {
+                System.out.println("Digite a temperatura em C");
+                try {
+                    double celsius = scanner.nextDouble();
+                    ConversorDeTemperaturas conversorDeTemperaturas = new ConversorDeTemperaturas();
+                    LocalDateTime start = LocalDateTime.now();
+                    LOGGER.info("Inicio do calculo de conversão");
+                    double fahrenheith = conversorDeTemperaturas.celsiusParaFahrenheith(celsius);
+                    LocalDateTime end = LocalDateTime.now();
+                    long between = ChronoUnit.MILLIS.between(start, end);
+                    LOGGER.debug("Final do metodo de conversão com tempo: " + between + " MS");
+                    System.out.println("A temperatura em K é: " + fahrenheith + "F");
+                } catch (InputMismatchException e) {
+                    LOGGER.error("Valor inválido para temperatura");
+                    System.out.println("Valor inválido para temperatura");
+                }
                 break;
             }
         }
